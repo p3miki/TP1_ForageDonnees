25def4038abaa844b9c571ab2a2bba2fa320da9e
p3miki <p3miki@outlook.com>
2020-02-04 12:18:02 -0500

    Further filter the data
    -Removed data from 2020 (not enough)
    -Removed data from 2017 (not enough)
    +Graphs now shows up in fullscreen by default

--- core/parser/parser.py
+++ core/parser/parser.py
@@ -6,4 +6,7 @@ def parse_data(data_frame):
         :param data_frame:
         :type data_frame: pandas.DataFrame"""
     data_frame.dropna(inplace=True)
+    data_frame = data_frame.loc[data_frame['Year'] != 2020]
+    data_frame = data_frame.loc[data_frame['Year'] != 2017]
+
     return data_frame
--- core/view/viewer.py
+++ core/view/viewer.py
@@ -1,12 +1,12 @@
+import os
+
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
 import pandas as pd
 import numpy as np
 
 
 def update_view(data_frame):
-    """Plots a graphs with the data provided"""
-    # df['Year'] = df['Year'].astype(int)
+    """Plots the graphs with the data provided"""
     afficher_regions(data_frame)
     afficher_globales(data_frame)
 
@@ -18,6 +18,7 @@ def afficher_globales(data_frame):
     plt.xlabel('Nombre Ventes Globales')
     plt.ylabel('Ventes (en millions)')
     plt.title('Ventes globales')
+    _maximize(plt.get_current_fig_manager(), plt)
     plt.show()
 
 
@@ -27,11 +28,23 @@ def afficher_regions(data_frame):
         :type data_frame: pandas.DataFrame"""
     years = data_frame['Year'].unique()
     years.sort()
-    dff = data_frame.groupby(['Year'])\
+    dff = data_frame.groupby(['Year']) \
         .agg({'NA_Sales': 'sum', 'EU_Sales': 'sum', 'JP_Sales': 'sum', 'Other_Sales': 'sum'})
     dff.plot(kind='bar', rot=1)
     plt.xlabel('Région De Vente')
     plt.xticks(np.arange(len(years)), pd.Series(years).apply(lambda x: "" if x % 4 else x))
     plt.ylabel('Ventes (en millions)')
     plt.title('Ventes par région')
+    _maximize(plt.get_current_fig_manager(), plt)
     plt.show()
+
+
+def _maximize(manager, plotter):
+    if os.uname().sysname == "Linux" and plotter.get_backend() == "TkAgg":
+        manager.resize(*manager.window.maxsize())
+    elif plotter.get_backend() == "TkAgg":
+        manager.window.state('zoomed')
+    elif plotter.get_backend() == "wxAgg":
+        manager.frame.Maximize(True)
+    elif plotter.get_backend() == "Qt4Agg":
+        manager.window.showMaximized()
