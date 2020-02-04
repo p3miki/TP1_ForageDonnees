045f43fe4e97f60141cdc7272846277f1e9d5ba3
p3miki <p3miki@outlook.com>
2020-02-04 11:39:28 -0500

    Increased visibility of the sales graph by region
    ~Fixed pylint

--- core/parser/parser.py
+++ core/parser/parser.py
@@ -2,9 +2,8 @@ import pandas
 
 
 def parse_data(data_frame):
-    """:param data_frame:
+    """Used to filter the data before returning the data frame
+        :param data_frame:
         :type data_frame: pandas.DataFrame"""
-    data_top = data_frame.head(10)
-    # iterating the columns
-    #print(data_frame)
-    return data_top
+    data_frame.dropna(inplace=True)
+    return data_frame
--- core/view/viewer.py
+++ core/view/viewer.py
@@ -1,26 +1,37 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
-import pandas
+import pandas as pd
+import numpy as np
 
 
-def update_view(df):
+def update_view(data_frame):
     """Plots a graphs with the data provided"""
-    #df['Year'] = df['Year'].astype(int)
-    afficher_regions(df)
-    afficher_globales(df)
+    # df['Year'] = df['Year'].astype(int)
+    afficher_regions(data_frame)
+    afficher_globales(data_frame)
 
-def afficher_globales(df):
-    dff = df.groupby(['Genre'])['Global_Sales'].sum()
+
+def afficher_globales(data_frame):
+    """Prints the global sales graph"""
+    dff = data_frame.groupby(['Genre'])['Global_Sales'].sum()
     dff.plot(kind='bar', rot=1)
     plt.xlabel('Nombre Ventes Globales')
     plt.ylabel('Ventes (en millions)')
-    plt.title('Ventes par région')
+    plt.title('Ventes globales')
     plt.show()
 
-def afficher_regions(df):
-    dff = df.groupby(['Year']).agg({'NA_Sales': 'sum', 'EU_Sales': 'sum', 'JP_Sales': 'sum', 'Other_Sales': 'sum'})
+
+def afficher_regions(data_frame):
+    """Prints sales graph by region
+        :param data_frame:
+        :type data_frame: pandas.DataFrame"""
+    years = data_frame['Year'].unique()
+    years.sort()
+    dff = data_frame.groupby(['Year'])\
+        .agg({'NA_Sales': 'sum', 'EU_Sales': 'sum', 'JP_Sales': 'sum', 'Other_Sales': 'sum'})
     dff.plot(kind='bar', rot=1)
     plt.xlabel('Région De Vente')
+    plt.xticks(np.arange(len(years)), pd.Series(years).apply(lambda x: "" if x % 4 else x))
     plt.ylabel('Ventes (en millions)')
     plt.title('Ventes par région')
     plt.show()
